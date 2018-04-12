890b6c358b95efc95c5e41fae3bc43e92049ba06
Alex <a.soldatov@deltasolutions.ru>
2018-04-12 12:19:32 +0300

    Delete webapp

---
+++ storage/src/main/java/ru/hse/lorddux/receiver/TCPReceiver.java
@@ -0,0 +1,14 @@
+package ru.hse.lorddux.receiver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.PreparedStatement;
+
+public class TCPReceiver {
+    public static void main(String[] args) {
+        Driver d;
+        PreparedStatement ps;
+        Connection c = d.connect("", null);
+        c.
+    }
+}
